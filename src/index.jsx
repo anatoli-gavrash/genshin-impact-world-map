83996d2be23a35cffdab39b526710a4b895a449d
GavrashAnatoly <gaver5@yandex.ru>
2021-08-06 23:47:20 +0300

    fix 4

--- src/index.jsx
+++ src/index.jsx
@@ -4,8 +4,8 @@ import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 
 import './index.scss';
-import App from './components/app/app.jsx';
-import storage from './storage/storage.jsx';
+import App from 'components/app/app.jsx';
+import storage from 'storage/storage.jsx';
 
 ReactDOM.render(
   <React.StrictMode>
