088fe3e1dc232c0dcdc079ec6091155ee5bd99a4
GavrashAnatoly <gaver5@yandex.ru>
2021-08-06 23:34:21 +0300

    fix 2

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import { HashRouter } from 'react-router-dom';
 
 import './index.scss';
 import App from 'components/app/app.jsx';
-import storage from 'storage/storage.jsx';
+import storage from './storage/storage.jsx';
 
 ReactDOM.render(
   <React.StrictMode>
